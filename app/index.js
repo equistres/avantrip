57c489497a64e4b5bbb60b1ba4dd4b2c6d8c19f4
Ale Soto <asoto@lanacion.com.ar>
2019-04-24 06:54:30 -0300

    Initial commit
    test commit

---
+++ app/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="container">
+          <div className="row">
+            <img src="https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1397193635/04d53b32c02a4751a2d182e357785176.png" />
+            <span className="mt-5 text-danger">Viajar es la guita mejor invertida</span>
+          </div>
+          <div className="row">aaaa</div>
+          <div className="row">bbbbbbb</div>
+        </div>
+      </>
+
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
+
+export default App;

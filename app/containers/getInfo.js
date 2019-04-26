df4689105783b99ac6b9e58b9b66383db6cf5f4d
Ale Soto <asoto@lanacion.com.ar>
2019-04-26 00:31:51 -0300

    migracion de clase a hook

--- app/containers/getInfo.js
+++ app/containers/getInfo.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import GetInfoComponent from '../components/getInfo';
 import Packs from '../containers/packs';
-import _ from 'lodash';
+
+let content;
 
 const axiosGraphQL = axios.create({
     baseURL: 'https://api.graph.cool/simple/v1/cjtk3okib547g0182680rna24',
@@ -25,85 +26,49 @@ const ALLCARDS = `{
     }
 }`;
 
+function useData(defaultResponse) {
+    const [data, newData] = useState(defaultResponse);
 
-class GetInfo extends Component {
-    state = {
-        data: null,
-        fetched: false,
-        hasFilter: false
-    }
-
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    fetchData = () => {
+    function getDataFromApi() {
         axiosGraphQL
             .post('', { query: ALLCARDS })
-            .then(result => this.setState({
-                stay: result.data.data.allStayDatas,
+            .then(result => newData({
                 data: result.data.data.allCards,
-                fetched: true,
-            }));
-    };
 
-    // groupBy(dataToGroupOn, fieldNameToGroupOn, fieldNameForGroupName, fieldNameForChildren) {
-    //     var result = _.chain(dataToGroupOn)
-    //         .groupBy(fieldNameToGroupOn)
-    //         .toPairs()
-    //         .map(function (currentItem) {
-    //             return _.zipObject([fieldNameForGroupName, fieldNameForChildren], currentItem);
-    //         })
-    //         .value();
-    //     return result;
-    // };
-
-    render() {
-        const { fetched, data, stay } = this.state;
-
-        if (fetched) {
-
-            // let filterByType = this.groupBy(data, 'imgUrl', 'destino', 'paquetes');
-            // filterByType = _.sortBy(filterByType, o => o.paquetes.price)
-
-            const orderedStay = _.sortBy(stay, o => o.customId)
-
-            // const cheaper = { destino: "Todos los destinos", paquetes: [orderedByPrice[0]] }
-            // filterByType.unshift(cheaper);
-
-            // const allData = this.state.data;
-
-            // if (this.state.hasFilter) {
-            //     allData = "";
-            // }
-
-
-            return (
-                <>
-                    <div className="container">
-                        <div className="row">
-                            <img src="https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1397193635/04d53b32c02a4751a2d182e357785176.png" />
-                            <span className="mt-5 text-danger">Viajar es la guita mejor invertida</span>
-                        </div>
-                        <div className="row">
-                            <nav className="navbar navbar-light bg-light">
-                                <form className="form-inline">                                    
-                                    <GetInfoComponent packs={orderedStay} />
-                                </form>
-                            </nav>
-                        </div>
-                        <div className="row"><Packs info={data} /></div>
-                    </div>
-                </>
-
-
-            )
-        } else {
-            return (
-                <p> Loading...</p>
-            )
-        }
-
+                stay: result.data.data.allStayDatas,
+                fetched: true
+            }))
     }
+
+    useEffect(() => {
+        getDataFromApi()
+    }, []);
+
+    return data;
 }
-export default GetInfo
+
+
+
+
+export default function GetInfo() {
+    content = useData({ data: null, stay: null, fetched: false })
+
+    return !content.fetched ? (
+        <div>loading</div>
+    ) : (
+            <div className="container">
+                <div className="row">
+                    <img src="https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1397193635/04d53b32c02a4751a2d182e357785176.png" />
+                    <span className="mt-5 text-danger">Viajar es la guita mejor invertida</span>
+                </div>
+                <div className="row">
+                    <nav className="navbar navbar-light bg-light">
+                        <form className="form-inline">
+                            <GetInfoComponent packs={content.stay} />
+                        </form>
+                    </nav>
+                </div>
+                <div className="row"><Packs info={content.data} /></div>
+            </div>
+        )
+}

3196cd776b9c65af6a52dbf26cf9aa819e99f96d
Ale Soto <asoto@lanacion.com.ar>
2019-04-28 11:05:11 -0300

    ,

--- src/components/stay.js
+++ src/components/stay.js
@@ -4,7 +4,7 @@ export default function Stay({ info, handleClick }) {
 
     const Cards = info.map(function (item, index) {
         return (
-            <button id={item.customId} onClick={handleClick} key={index} className="btn btn-sm btn-outline-secondary mr-4" type="button">{item.label} <br />desde ${item.bestPrice}</button>
+            <button id={item.customId} onClick={handleClick} key={index} className="btn btn-light btn-sm btn-outline-secondary mr-4" type="button" style={{height:'60px'}}>{item.label} <br />desde ${item.bestPrice}</button>
         )
     })
     return (
